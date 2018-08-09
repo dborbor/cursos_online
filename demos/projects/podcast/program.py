5c4410f767e2edd52358deead4d41e3465e87048
Daniel Borbor <dborbor@gmail.com>
2018-08-09 19:03:14 -0400

    Later episodes

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -17,8 +17,8 @@ def show_header():
 
 def display_results():
 
-    start = random.randint(90, 110)
-    end = random.randint(130, service.get_latest_show_id()+1)
+    start = random.randint(90, 105)
+    end = random.randint(121, service.get_latest_show_id()+1)
 
     for show_id in range(start, end):
         info = service.get_episode(show_id)
